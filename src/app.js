71bf9aa3c727ded9136df26ae31c52549dc687bc
Nyberg5000 <82217232+Nyberg5000@users.noreply.github.com>
2021-05-25 20:01:54 +0200

    Added: axios; JS functions; changed lists span in HTML

--- src/app.js
+++ src/app.js
@@ -1,1 +1,42 @@
+function showTemperature(response) {
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#city-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("rain").innerHTML=response.data.wind.
+}
 
+function getCity(city) {
+  let apiKey = "6564272724482451c8ea4a6b9bde60dd";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function findCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-name").value;
+  getCity(city);
+}
+
+let searchCity = document.querySelector("#city-search");
+searchCity.addEventListener("submit", findCity);
+
+let todayDate = document.querySelector("#date");
+let currentDate = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentDay = days[currentDate.getDay()];
+let hours = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+todayDate.innerHTML = `${currentDay} ${hours}:${minutes}`;
